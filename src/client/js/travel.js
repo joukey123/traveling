f1914d57b956e064b9aaaa86bd9c76a8199c034d
joukey123 <0162943362@naver.com>
2023-11-06 13:49:29 +0900

    Youtube API add

--- src/client/js/travel.js
+++ src/client/js/travel.js
@@ -3,37 +3,35 @@ const recCityContents = document.querySelector(".recCityContents");
 const recCityBoxs = document.querySelectorAll(".recCityBox");
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
+const childrens = recCityContents.children;
 
 let imgNum = 0;
 
 //선택된 이미지
-const handleSelectCity = (a) => {
-  const childrens = recCityContents.children;
+const handleSelectCity = () => {
   const textBoxs = document.querySelectorAll(".recCityDef");
   for (let textBox of textBoxs) {
     textBox.classList.add("imgTextHidden");
   }
   for (let children of childrens) {
-    if (a) {
-      children.className = "recCityBox";
-    }
+    children.className = "recCityBox";
   }
   childrens[2].childNodes[1].classList.remove("imgTextHidden");
-  childrens[2].classList.add(`${a}big`);
+  childrens[2].classList.add(`big`);
 };
 
-//뒤로보냄
+//뒤로보냄 //왼쪽버튼
 const handleAppendMoveImg = () => {
   recCityContents.insertAdjacentElement("beforeend", recCityBoxs[imgNum]);
-  handleSelectCity("right");
+  handleSelectCity();
 };
 
-//앞으로보냄
+//앞으로보냄 //오른쪽버튼
 const handlePrependMoveImg = () => {
   recCityContents.insertAdjacentElement("afterbegin", recCityBoxs[imgNum]);
-  handleSelectCity("left");
+  handleSelectCity();
 };
-
+handleAppendMoveImg;
 const handlePrevCity = () => {
   handleAppendMoveImg();
   imgNum++;
--- src/controller/travelController.js
+++ src/controller/travelController.js
@@ -1,2 +1,25 @@
 export const home = (req, res) => res.render("home");
-export const travel = (req, res) => res.render("travel");
+export const travel = (req, res) => {
+  var YouTube = require("youtube-node");
+  const youtube = new YouTube();
+  const word = "뉴질랜드 여행";
+  const limit = 3;
+
+  //   youtube.setKey("AIzaSyBFt0Le4tsDRChIip8_AUevMr7lrt8B9cI"); //크로크무슈
+  youtube.setKey("AIzaSyD-ynXsP4uQ4Mz4nXdz53qwwQ0awljbFdo"); //holland
+
+  youtube.addParam("order", "rating");
+  youtube.addParam("type", "video");
+  youtube.addParam("videoLicense", "creativeCommon");
+
+  youtube.search(word, limit, function (err, result) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const videos = result["items"];
+
+    res.render("travel", { videos });
+  });
+};
